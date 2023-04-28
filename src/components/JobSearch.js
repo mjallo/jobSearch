b586c96d7cc9f7fbd34fc93851d1c51f6b6491e5
mjallo <90021166+mjallo@users.noreply.github.com>
2023-04-28 15:52:30 +0200

    inline css

--- src/components/JobSearch.js
+++ src/components/JobSearch.js
@@ -3,11 +3,34 @@ import NavigationBar from "./NavigationBar";
 
 function JobSearch() {
   return (
-    <div>
+    <div style={{ backgroundColor: "#f4f4f4", padding: "2rem" }}>
       <NavigationBar />
-      <h1>Job Search</h1>
-      <input placeholder="Search for jobs" />
-      <button>Search</button>
+      <h1 style={{ fontSize: "3rem", marginBottom: "2rem" }}>Job Search</h1>
+      <form style={{ display: "flex" }}>
+        <input
+          style={{
+            padding: "1rem",
+            border: "none",
+            borderRadius: "5px 0 0 5px",
+            flex: "1",
+            marginRight: "-1px"
+          }}
+          placeholder="Search for jobs"
+        />
+        <button
+          style={{
+            backgroundColor: "#00b8d4",
+            color: "#fff",
+            padding: "1rem",
+            border: "none",
+            borderRadius: "0 5px 5px 0",
+            cursor: "pointer",
+            transition: "all 0.2s ease-in-out"
+          }}
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
